8d70b47b2f648f875958bbcbd620d071bbe50963
siddeshnaik <siddeshnaik817@gmail.com>
2022-10-26 12:36:59 +0530

    Commiting another practice trail of recyclerView

---
+++ app/src/main/java/com/example/recyclerview3/Datasource.kt
@@ -0,0 +1,10 @@
+package com.example.recyclerview3
+
+import android.content.Context
+
+class Datasource(val context: Context) {
+    fun getFlowerList(): Array<String>{
+        return context.resources.getStringArray(R.array.flower_array)
+
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview3/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.recyclerview3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flowerList = Datasource(this).getFlowerList()
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = FlowerAdapter(flowerList)
+
+
+    }
+}
